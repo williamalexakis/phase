439bbeeabe701070053293101bc9e9309210d1eb
zchroma <222100474+zchroma@users.noreply.github.com>
2025-08-20 20:07:17 +0300

    Feat: Implement basic file reading

--- transpiler.c
+++ transpiler.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void) {
+int main(int argc, char **argv) {
 
-    printf("PLACEHOLDER\n");
+    // Check if not enough args or help flag is used
+    if (argc < 2 || strcmp(argv[1], "--help") == 0) {
+
+        // Display error and exit
+        printf("[zirc] USAGE: zirc <input.zir> <output.c>\n");
+        exit(1);
+
+    }
+
+    FILE *input_file = fopen(argv[1], "r");  // Open the input file
+
+    // Check if file is not found
+    if (input_file == NULL) {
+
+        // Display error and exit
+        printf("[zirc] ERROR: INPUT FILE '%s' NOT FOUND\n", argv[1]);
+        exit(1);
+
+    }
+
+    unsigned int temp_buffer = 256;
+    char input_content[temp_buffer];
+
+    // Read file content and print it
+    while (fgets(input_content, temp_buffer, input_file)) {
+
+            printf("%s", input_content);
+
+    }
+
+    fclose(input_file);  // Close the input file
 
     return 0;
 
