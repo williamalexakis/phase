e76c8a7df12692239de9a3f72e98bdbc6339cb0a
zchroma <222100474+zchroma@users.noreply.github.com>
2025-08-22 17:36:01 +0300

    Refactor: Read to and display allocated buffer for input file content

--- transpiler.c
+++ transpiler.c
@@ -1,16 +1,23 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
 
-    // Check if not enough args provided or
-    // the help flag is used
-    if (argc < 2 || strcmp(argv[1], "--help") == 0) {
+    // Check if not enough args are provided
+    if (argc < 2) {
 
-        // Display error and exit
-        printf("[zirc] USAGE: zirc <input.zir> <output.c>\n");
+        fprintf(stderr, "[zirc] ERROR: INSUFFICIENT ARGUMENTS PROVIDED\n");
         exit(1);
+
+    }
+
+    // Check if help flag is used
+    if (strcmp(argv[1], "--help") == 0) {
+
+        fprintf(stderr, "[zirc] USAGE: zirc <input.zir> <output.c>\n");
+        exit(0);
 
     }
 
@@ -19,23 +26,38 @@ int main(int argc, char **argv) {
     // Check if file is not found
     if (input_file == NULL) {
 
-        // Display error and exit
-        printf("[zirc] ERROR: INPUT FILE '%s' NOT FOUND\n", argv[1]);
+        fprintf(stderr, "[zirc] ERROR: INPUT FILE '%s' NOT FOUND\n", argv[1]);
         exit(1);
 
     }
 
-    unsigned int max_line_size = 256;
-    char line_buffer[max_line_size];
+    // Figure out the file size by moving the file ptr to the end
+    fseek(input_file, 0, SEEK_END);
+    size_t file_size = (size_t)ftell(input_file);
+    rewind(input_file); // Return file ptr to start
 
-    // Print each line of the file until EOF reached
-    while (fgets(line_buffer, max_line_size, input_file)) {
+    // Allocate buffer large enough for the file + null terminator
+    char *file_content = malloc(file_size + 1);
 
-            printf("%s", line_buffer);
+    if (!file_content) {
+
+        fprintf(stderr, "[zirc] ERROR: OUT OF MEMORY\n");
+        free(file_content);
+        fclose(input_file);
+
+        exit(1);
 
     }
 
-    fclose(input_file);  // Close the input file
+    // Read the whole file into the buffer
+    fread(file_content, sizeof(char), file_size, input_file);
+    file_content[file_size] = '\0';  // Append null terminator
+
+    fclose(input_file);
+
+    printf("%s\n", file_content);  // Display the file content
+
+    free(file_content);
 
     return 0;
 
